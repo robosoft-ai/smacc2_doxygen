09f703a357662644516f9037459f93fb9f127758
pabloinigoblasco <pabloinigoblasco@users.noreply.github.com>
2021-10-06 20:32:07 +0000

    aws demo (#108)
    * aws demo
    
    * format robosoft-ai/SMACC2@5e4ba4c6fcf48b792ce93a65fdaf62a585da85d8

--- master/html/search/pages_2.js
+++ master/html/search/pages_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['empty_20room_4669',['Empty Room',['../md_smacc2_sm_reference_library_sm_dance_bot_lite_models_empty_room_README.html',1,'(Global Namespace)'],['../md_smacc2_sm_reference_library_sm_dance_bot_models_empty_room_README.html',1,'(Global Namespace)'],['../md_smacc2_sm_reference_library_sm_dance_bot_strikes_back_models_empty_room_README.html',1,'(Global Namespace)']]]
+  ['empty_20room_4669',['Empty Room',['../md_smacc2_sm_reference_library_sm_aws_warehouse_navigation_models_empty_room_README.html',1,'(Global Namespace)'],['../md_smacc2_sm_reference_library_sm_dance_bot_lite_models_empty_room_README.html',1,'(Global Namespace)'],['../md_smacc2_sm_reference_library_sm_dance_bot_models_empty_room_README.html',1,'(Global Namespace)'],['../md_smacc2_sm_reference_library_sm_dance_bot_strikes_back_models_empty_room_README.html',1,'(Global Namespace)']]]
 ];
