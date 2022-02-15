7f2b77697b25896473c426336b74f69e9ec06d17
destogl <destogl@users.noreply.github.com>
2022-02-15 08:09:32 +0000

    Only galactic pre-release test on galactic branch (#231) robosoft-ai/SMACC2@12631ac06a5795017086ebcad0017fd7008aad82

--- galactic/html/search/classes_a.js
+++ galactic/html/search/classes_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['ledactionserver_2340',['LEDActionServer',['../classLEDActionServer.html',1,'']]]
+  ['ledactionserver_0',['LEDActionServer',['../classLEDActionServer.html',1,'']]]
 ];
