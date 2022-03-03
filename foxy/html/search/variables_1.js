4fe667747965b17bc523d4e4501affb7c7c49acc
pabloinigoblasco <pabloinigoblasco@users.noreply.github.com>
2022-03-03 20:44:02 +0000

    backport galactic to foxy and fixing source build (#237) robosoft-ai/SMACC2@1eea122fa3acbe778413750adffc7607aa9346cb

--- foxy/html/search/variables_1.js
+++ foxy/html/search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['args_0',['args',['../namespacegenerate__debs.html#a75f9143e38df82d83b2e8a6f99cae02c',1,'generate_debs']]],
-  ['attachedclient_5f_1',['attachedClient_',['../classsmacc2_1_1client__behaviors_1_1CbSubscriptionCallbackBase.html#a1dfadf791f4f4ad5fe85a774d8e0bbb7',1,'smacc2::client_behaviors::CbSubscriptionCallbackBase']]]
+  ['action_5fendpoint_5f_0',['action_endpoint_',['../classsmacc2_1_1client__bases_1_1SmaccActionClientBase.html#a2f7480cb7236ede7ce04d71752daf518',1,'smacc2::client_bases::SmaccActionClientBase']]],
+  ['args_1',['args',['../namespacegenerate__debs.html#a75f9143e38df82d83b2e8a6f99cae02c',1,'generate_debs']]],
+  ['attachedclient_5f_2',['attachedClient_',['../classsmacc2_1_1client__behaviors_1_1CbSubscriptionCallbackBase.html#a1dfadf791f4f4ad5fe85a774d8e0bbb7',1,'smacc2::client_behaviors::CbSubscriptionCallbackBase']]]
 ];
--- foxy/html/search/variables_a.js
+++ foxy/html/search/variables_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['k_0',['k',['../structsm__pubsub__1_1_1StState2.html#a1fa5eb1f1f418b5737da2523522faf50',1,'sm_pubsub_1::StState2']]]
+  ['k_0',['k',['../structsm__ferrari_1_1StState2.html#af43721743ea34cc998b16bc2857f85c3',1,'sm_ferrari::StState2::k()'],['../structsm__pubsub__1_1_1StState2.html#a1fa5eb1f1f418b5737da2523522faf50',1,'sm_pubsub_1::StState2::k()']]]
 ];
