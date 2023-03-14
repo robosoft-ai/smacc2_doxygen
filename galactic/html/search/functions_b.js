8057babf8794edffcadbf4594727674c835ceffe
brettpac <brettpac@users.noreply.github.com>
2023-03-14 16:13:54 +0000

    Exitting to Exiting (#431) robosoft-ai/SMACC2@e8d9239e75c05018d6d5848e6b0670d3ea586da6

--- galactic/html/search/functions_b.js
+++ galactic/html/search/functions_b.js
@@ -6,7 +6,7 @@ var searchData=
   ['notifyonstateentryend_3',['notifyOnStateEntryEnd',['../classsmacc2_1_1ISmaccStateMachine.html#a80d1d07569569bddd2f8fab8d5f82e58',1,'smacc2::ISmaccStateMachine']]],
   ['notifyonstateentrystart_4',['notifyOnStateEntryStart',['../classsmacc2_1_1ISmaccStateMachine.html#afcf1bd46804d7e1b0784067b0f8339dd',1,'smacc2::ISmaccStateMachine']]],
   ['notifyonstateexited_5',['notifyOnStateExited',['../classsmacc2_1_1ISmaccStateMachine.html#a73ee6bc299f74724ad5c734b55e544c5',1,'smacc2::ISmaccStateMachine']]],
-  ['notifyonstateexitting_6',['notifyOnStateExitting',['../classsmacc2_1_1ISmaccStateMachine.html#a380ecb6f3767060cbc4447e6c814d917',1,'smacc2::ISmaccStateMachine']]],
+  ['notifyonstateexiting_6',['notifyOnStateExiting',['../classsmacc2_1_1ISmaccStateMachine.html#ac0fabeda812dbd41ef3ec29376632cb3',1,'smacc2::ISmaccStateMachine']]],
   ['notifytransition_7',['notifyTransition',['../classsmacc2_1_1ISmaccState.html#ad5349c83f7a9455a07113e088ad295d3',1,'smacc2::ISmaccState']]],
   ['notifytransitionfromtransitiontypeinfo_8',['notifyTransitionFromTransitionTypeInfo',['../classsmacc2_1_1ISmaccState.html#af07000235dbc0541fe4cd9cc08790dd1',1,'smacc2::ISmaccState']]]
 ];
