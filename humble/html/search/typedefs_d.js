5c8caeaf25a458f76742dda9990a6492bbe7ff62
brettpac <brettpac@users.noreply.github.com>
2025-09-17 03:16:27 +0000

    Refactoring cl_nav2z to remove legacy api support and update all client behaviors (#625)
    Co-authored-by: brettpac <brettpac@system76-pc.localdomain> robosoft-ai/SMACC2@eb5b38a98e0208f3f6184eea2a20603640b9597b

--- humble/html/search/typedefs_d.js
+++ humble/html/search/typedefs_d.js
@@ -7,6 +7,6 @@ var searchData=
   ['smaccallocator_4',['SmaccAllocator',['../namespacesmacc2_1_1introspection.html#ab06f75f2256ec0fed683f3077245cfc6',1,'smacc2::introspection::SmaccAllocator'],['../smacc__fifo__worker_8hpp.html#ab10968218a87a8bd294866c1634fab4b',1,'SmaccAllocator:&#160;smacc_fifo_worker.hpp']]],
   ['smaccfifoscheduler_5',['SmaccFifoScheduler',['../smacc__fifo__scheduler_8hpp.html#a0063e275231c80d5f97df21d17257bf7',1,'smacc_fifo_scheduler.hpp']]],
   ['smaccfifoworker_6',['SmaccFifoWorker',['../smacc__fifo__worker_8hpp.html#afb691eb7bc303fb14ab6e3c3e82729a3',1,'smacc_fifo_worker.hpp']]],
-  ['smaccnavigateresultsignal_7',['SmaccNavigateResultSignal',['../classcl__nav2z_1_1ClNav2Z.html#ac19906f84698113fc5d5a604e1da015d',1,'cl_nav2z::ClNav2Z']]],
+  ['smaccnavigateresultsignal_7',['SmaccNavigateResultSignal',['../classcl__nav2z_1_1ClNav2Z.html#adbb44029cc66ae70800956e0d5d253dc',1,'cl_nav2z::ClNav2Z']]],
   ['state_5fiterator_8',['state_iterator',['../classsmacc2_1_1SmaccState.html#af37e424c67735b16565101e3738c6822',1,'smacc2::SmaccState']]]
 ];
--- humble/html/search/variables_0.js
+++ humble/html/search/variables_0.js
@@ -3,7 +3,7 @@ var searchData=
   ['abortednav2zclientconnection_5f_0',['abortedNav2ZClientConnection_',['../classcl__nav2z_1_1CpWaypointNavigator.html#a5a6028838a8623abe8433bc0206b89d7',1,'cl_nav2z::CpWaypointNavigator']]],
   ['absolutegoalangledegree_1',['absoluteGoalAngleDegree',['../classcl__nav2z_1_1CbAbsoluteRotate.html#aadd82e7bc5fdf1a44a0b3da847a44f0e',1,'cl_nav2z::CbAbsoluteRotate']]],
   ['action_5fendpoint_5f_2',['action_endpoint_',['../classsmacc2_1_1client__bases_1_1SmaccActionClientBase.html#a2f7480cb7236ede7ce04d71752daf518',1,'smacc2::client_bases::SmaccActionClientBase']]],
-  ['actionclient_5f_3',['actionClient_',['../classcl__nav2z_1_1components_1_1CpNav2ActionInterface.html#ab3cabdc103e2f8ac1259e0dbdb96e185',1,'cl_nav2z::components::CpNav2ActionInterface']]],
+  ['actionclient_5f_3',['actionClient_',['../classcl__nav2z_1_1CbNav2ZClientBehaviorBase.html#a0f47314caeda82be57c5d329c5b44e6b',1,'cl_nav2z::CbNav2ZClientBehaviorBase::actionClient_'],['../classcl__nav2z_1_1components_1_1CpNav2ActionInterface.html#ab3cabdc103e2f8ac1259e0dbdb96e185',1,'cl_nav2z::components::CpNav2ActionInterface::actionClient_']]],
   ['actionmutex_5f_4',['actionMutex_',['../classsmacc2_1_1client__core__components_1_1CpActionClient.html#a806cfb164cf4bd9c87ff5e95ba95a042',1,'smacc2::client_core_components::CpActionClient']]],
   ['actionservername_5',['actionServerName',['../classsmacc2_1_1client__core__components_1_1CpActionClient.html#a0cbd335fa72b603f0f06257b3beed17d',1,'smacc2::client_core_components::CpActionClient']]],
   ['actionservername_5f_6',['actionServerName_',['../classcl__nav2z_1_1ClNav2Z.html#a3ccd34450765786ca4503130e4dd0dfd',1,'cl_nav2z::ClNav2Z']]],
